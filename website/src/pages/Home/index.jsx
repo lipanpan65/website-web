5a4a918e6114bfca08963998316144a9c7ad6fe5
lipanpan03 <lipanpan03@58.com>
2021-07-31 22:19:12 +0800

    feat-Home页面-调整布局

--- website/src/pages/Home/index.jsx
+++ website/src/pages/Home/index.jsx
@@ -1,11 +1,37 @@
 
 import React, { Component } from 'react';
-import { Layout, Menu, Row, Col } from 'antd';
+import { Layout, Menu, Row, Col, Card, Breadcrumb, List, Icon } from 'antd';
 import "./style.css"
 
 // Breadcrumb
+// Avatar
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
+
+// Footer
+
+const data = [
+    {
+        title: 'Ant Design Title 1',
+    },
+    {
+        title: 'Ant Design Title 2',
+    },
+    {
+        title: 'Ant Design Title 3',
+    },
+    {
+        title: 'Ant Design Title 4',
+    },
+];
+
+
+const IconText = ({ type, text }) => (
+    <span>
+        <Icon type={type} style={{ marginRight: 8 }} />
+        {text}
+    </span>
+);
 
 class Home extends Component {
     constructor(props) {
@@ -16,18 +42,22 @@ class Home extends Component {
         return (
             <div>
                 <Layout className="layout">
-                    <Header>
-                        <div className="logo" />
-                        <Menu
-                            theme="dark"
-                            mode="horizontal"
-                            defaultSelectedKeys={['1']}
-                            style={{ lineHeight: '64px' }}
-                        >
-                            <Menu.Item key="1">首页</Menu.Item>
-                            <Menu.Item key="2">nav 2</Menu.Item>
-                            <Menu.Item key="3">nav 3</Menu.Item>
-                        </Menu>
+                    <Header className="home-header">
+                        <Row type="flex" justify="center" align="middle" style={{ padding: '0 50px' }}>
+                            <Col span={16}>
+                                <div className="logo" />
+                                <Menu
+                                    theme="dark"
+                                    mode="horizontal"
+                                    defaultSelectedKeys={['1']}
+                                    style={{ lineHeight: '64px' }}
+                                >
+                                    <Menu.Item key="1">首页</Menu.Item>
+                                    {/* <Menu.Item key="2">nav 2</Menu.Item> */}
+                                    {/* <Menu.Item key="3">nav 3</Menu.Item> */}
+                                </Menu>
+                            </Col>
+                        </Row>
                     </Header>
                     <Content style={{ padding: '0 50px' }}>
                         {/* <Breadcrumb style={{ margin: '16px 0' }}>
@@ -38,22 +68,73 @@ class Home extends Component {
                         {/* <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
 
                         </div> */}
-                        <Row style={{ margin: '0 auto' }}>
-                            <Col span={10}>
-                                <div style={{
+                        <Row type="flex" justify="center"  style={{}}>
+                        {/* align="middle" */}
+                            <Col span={12}>
+                                <Card size="default" title={
+                                    <Breadcrumb separator="|">
+                                        <Breadcrumb.Item>
+                                            热门
+                                        </Breadcrumb.Item>
+                                        <Breadcrumb.Item>
+                                            最新
+                                        </Breadcrumb.Item>
+                                        <Breadcrumb.Item>
+                                            热榜
+                                        </Breadcrumb.Item>
+                                    </Breadcrumb>
+                                } style={{ minHeight: 280, marginRight: 16 }}
+                                    bodyStyle={{ padding: '0 24px' }}
+                                >
+
+                                    <List
+                                        // itemLayout="horizontal"
+                                        itemLayout="vertical"
+                                        dataSource={data}
+                                        renderItem={item => (
+                                            <List.Item
+                                            key={item.title}
+                                                actions={[
+                                                    <IconText type="star-o" text="156" key="list-vertical-star-o" />,
+                                                    <IconText type="like-o" text="156" key="list-vertical-like-o" />,
+                                                    <IconText type="message" text="2" key="list-vertical-message" />,
+                                                ]}
+                                            >
+                                                <List.Item.Meta
+                                                    // avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                                    title={<a href="https://ant.design">{item.title}</a>}
+                                                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                                />
+                                            </List.Item>
+                                        )}
+                                    />
+                                </Card>
+                                {/* <div style={{
                                     background: '#fff',
-                                    padding: 24, minHeight: 280,
+                                    // padding: 24, 
+                                    minHeight: 280,
                                     marginRight: 16
                                 }}>
+                                    <Card size="small" title="Small size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+
+
+                                    </Card>
                                     test
-                                </div>
+                                </div> */}
                             </Col>
                             <Col span={4}>
-                                <div style={{ background: 'red', padding: 24, minHeight: 280 }}></div>
+                                {/* <div style={{ background: 'red', padding: 24, }}></div> */}
+                                <Card
+                                size="default"
+                                style={{ minHeight: 200, marginRight: 16 }}
+                                >
+
+                                </Card>
+
                             </Col>
                         </Row>
                     </Content>
-                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                    {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
                 </Layout>
             </div>
         );
